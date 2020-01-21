b88e31179bb747ff94d170d90a62ad10e38552ca
Chris Hill-Scott <me@quis.cc>
2020-01-21 11:27:20 +0000

    Add some pages and apply service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Apply for a blue badge',
 
   // Default port that prototype runs on
   port: '3000',
