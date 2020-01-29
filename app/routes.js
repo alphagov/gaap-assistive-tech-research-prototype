e1a62ae895de375719427348a10015684bf0d43e
Chris Hill-Scott <me@quis.cc>
2020-01-29 11:39:43 +0000

    Integrate Notify to send emails to participants
    Adds /backstage which people running research sessions can use to
    trigger the sending of emails during sessions.
    
    Getting this running locally requires having the environment variable
    `NOTIFY_API_KEY` set.
    
    Mostly copy/pasted from:
    - https://govuk-prototype-kit.herokuapp.com/docs/using-notify
    - https://docs.notifications.service.gov.uk/node.html#send-a-file-by-email

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,76 @@
 const express = require('express')
+const fs = require('fs')
+const NotifyClient = require('notifications-node-client').NotifyClient
+const path = require('path');
+const notifyClient = new NotifyClient(process.env.NOTIFY_API_KEY)
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+
+router.get('/backstage', function(req, res) {
+    res.render('backstage/index.html');
+});
+
+
+router.post('/backstage/send-invite', function(req, res) {
+
+  notifyClient.sendEmail(
+    '5d54c4a8-ad5e-4612-9be1-f99d0fedbcfe',
+    req.session.data['email-address'],
+    {
+      personalisation: {}
+    }
+  ).then(response => {
+    res.redirect('/backstage/invite-sent');
+  }).catch(err => {
+    console.error(err.error.errors)
+    res.render('backstage/error.html', {
+      error: err.error.errors[0]
+    });
+  })
+
+});
+
+
+router.get('/backstage/invite-sent', function(req, res) {
+  res.render('backstage/invite-sent.html', {
+    email: req.session.data['email-address']
+  });
+});
+
+
+router.post('/backstage/send-document', function(req, res) {
+
+  fs.readFile(
+    path.resolve(__dirname, '../temp-blue-badge-v1.pdf'),
+    function (err, pdfFile) {
+      console.log(err)
+      notifyClient.sendEmail(
+        'a44131f2-474b-4da9-a1fd-7ff7d83d48ad',
+        req.session.data['email-address'],
+        {
+          personalisation: {
+            file: notifyClient.prepareUpload(pdfFile)
+          }
+        }
+      ).then(response => {
+        res.redirect('/backstage/document-sent');
+      }).catch(err => {
+        console.error(err.error.errors)
+        res.render('backstage/error.html', {
+          error: err.error.errors[0]
+        });
+      })
+    }
+  );
+
+});
+
+
+router.get('/backstage/document-sent', function(req, res) {
+  res.render('backstage/document-sent.html', {
+    email: req.session.data['email-address']
+  });
+});
+
 
 module.exports = router
