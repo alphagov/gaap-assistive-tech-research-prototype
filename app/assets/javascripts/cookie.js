7ef12f732baf3e872adee417605fb81fd0b33e25
Chris Hill-Scott <me@quis.cc>
2020-04-14 16:16:38 +0100

    Add a cookie banner
    This commit adds a cookie banner, which I have pilfered from the one
    used on GOV.UK Publishing.
    
    I chose this one because it’s the one viewed by the most people, so any
    recommendations we make about it would have the biggest value.
    
    It’s also likely to be the starting point for what services decide to
    copy, in the absence of a component in the Design System.
    
    I personally want to test this because I have strong reckons about the
    negative impact on usability that cookie banners have, especially when
    services have seen a majority of users ignore the banner rather than
    dismiss it, therefore leading to it appearing on every. Single. Page.
    Load. I reckon there is a real trade off to be made between the
    usability of a service and the value of setting non-essential cookies.
    If this is the case then it would be good for us to have done the
    research to help qualify this trade off.
    
    I have chosen to link to the help pages on GOV.UK if the user clicks any
    of the links in the banner. This does take people out of the prototype
    and we could consider making these links non-functional. But I feel that
    the risk of people getting waylaid is true using a real service as well,
    and we should see if this happens to people using our fake service.

---
+++ app/assets/javascripts/cookie.js
@@ -0,0 +1,37 @@
+// Extracted from
+// https://github.com/alphagov/govuk_publishing_components/blob/master/app/assets/javascripts/govuk_publishing_components/lib/cookie-functions.js
+
+window.GOVUK.cookie = function (name, value, options) {
+  if (typeof value !== 'undefined') {
+    if (typeof options === 'undefined') {
+      options = {}
+    }
+    var cookieString = name + '=' + value + '; path=/'
+    if (options.days) {
+      var date = new Date()
+      date.setTime(date.getTime() + (options.days * 24 * 60 * 60 * 1000))
+      cookieString = cookieString + '; expires=' + date.toGMTString()
+    }
+    if (document.location.protocol === 'https:') {
+      cookieString = cookieString + '; Secure'
+    }
+    document.cookie = cookieString
+  } else {
+    return window.GOVUK.getCookie(name)
+  }
+};
+
+window.GOVUK.getCookie = function (name) {
+  var nameEQ = name + '='
+  var cookies = document.cookie.split(';')
+  for (var i = 0, len = cookies.length; i < len; i++) {
+    var cookie = cookies[i]
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1, cookie.length)
+    }
+    if (cookie.indexOf(nameEQ) === 0) {
+      return decodeURIComponent(cookie.substring(nameEQ.length))
+    }
+  }
+  return null
+}
